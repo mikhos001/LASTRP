3102881e52050cea8aa2d9b2889f2019d963604e
mikh18rus <43314361+mikh18rus@users.noreply.github.com>
2025-02-16 15:27:48 +0400

    remove player armour assignment on death event

--- packages/events.js
+++ packages/events.js
@@ -3,5 +3,4 @@ let spawnPoints = require('./spawn_points.json').SpawnPoints;
 mp.events.add('playerDeath', (player) => {
     player.spawn(spawnPoints[Math.floor(Math.random() * spawnPoints.length)]);
     player.health = 100;
-    player.armour = 50;
 });
